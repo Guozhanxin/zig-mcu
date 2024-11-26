2f1228e8e303a089bb7cc6d8677473dcb1f1df27
guozhanxin <guozhanxin@rt-thread.com>
2024-11-26 16:50:37 +0800

    add app example

--- build.zig
+++ build.zig
@@ -7,46 +7,51 @@ const stm32f4 = std.zig.CrossTarget{
     .abi = .eabihf,
 };
 
+const examples = [_]Example{
+    .{ .name = "stm32-app", .target = stm32f4, .root_file = "src/app_main.zig", .linker_script = "src/chip/link_app.lds" },
+    .{ .name = "stm32-zboot", .target = stm32f4, .root_file = "src/main.zig", .linker_script = "src/chip/link.lds" },
+};
+
 pub fn build(b: *std.Build) void {
     const optimize = .ReleaseSmall;
-    const target_name = "stm32-zboot";
 
-    const elf = b.addExecutable(.{
-        .name = b.fmt("{s}{s}", .{ target_name, ".elf" }),
-        .root_source_file = b.path("src/main.zig"),
-        .target = b.resolveTargetQuery(stm32f4),
-        .optimize = optimize,
-        .strip = false, // do not strip debug symbols
-    });
+    for (examples) |example| {
+        const elf = b.addExecutable(.{
+            .name = b.fmt("{s}{s}", .{ example.name, ".elf" }),
+            .root_source_file = b.path(example.root_file),
+            .target = b.resolveTargetQuery(stm32f4),
+            .optimize = optimize,
+            .strip = false, // do not strip debug symbols
+        });
 
-    elf.setLinkerScript(b.path("src/chip/link.lds"));
-    elf.addCSourceFile(.{ .file = b.path("src/chip/start.s"), .flags = &.{} });
+        elf.setLinkerScript(b.path(example.linker_script));
+        elf.addCSourceFile(.{ .file = b.path("src/chip/start.s"), .flags = &.{} });
 
-    // Copy the elf to the output directory.
-    const copy_elf = b.addInstallArtifact(elf, .{});
-    b.default_step.dependOn(&copy_elf.step);
+        // Copy the elf to the output directory.
+        const copy_elf = b.addInstallArtifact(elf, .{});
+        b.default_step.dependOn(&copy_elf.step);
 
-    // Convert the hex from the elf
-    const hex = b.addObjCopy(elf.getEmittedBin(), .{ .format = .hex });
-    hex.step.dependOn(&elf.step);
-    // Copy the hex to the output directory
-    const copy_hex = b.addInstallBinFile(
-        hex.getOutput(),
-        b.fmt("{s}{s}", .{ target_name, ".hex" }),
-    );
-    b.default_step.dependOn(&copy_hex.step);
+        // Convert the hex from the elf
+        const hex = b.addObjCopy(elf.getEmittedBin(), .{ .format = .hex });
+        hex.step.dependOn(&elf.step);
+        // Copy the hex to the output directory
+        const copy_hex = b.addInstallBinFile(
+            hex.getOutput(),
+            b.fmt("{s}{s}", .{ example.name, ".hex" }),
+        );
+        b.default_step.dependOn(&copy_hex.step);
 
-    // Convert the bin form the elf
-    const bin = b.addObjCopy(elf.getEmittedBin(), .{ .format = .bin });
-    bin.step.dependOn(&elf.step);
+        // Convert the bin form the elf
+        const bin = b.addObjCopy(elf.getEmittedBin(), .{ .format = .bin });
+        bin.step.dependOn(&elf.step);
 
-    // Copy the bin to the output directory
-    const copy_bin = b.addInstallBinFile(
-        bin.getOutput(),
-        b.fmt("{s}{s}", .{ target_name, ".bin" }),
-    );
-    b.default_step.dependOn(&copy_bin.step);
-
+        // Copy the bin to the output directory
+        const copy_bin = b.addInstallBinFile(
+            bin.getOutput(),
+            b.fmt("{s}{s}", .{ example.name, ".bin" }),
+        );
+        b.default_step.dependOn(&copy_bin.step);
+    }
     const zboot_tool = b.addExecutable(.{
         .name = "zboot",
         .optimize = .ReleaseSmall,
@@ -56,3 +61,10 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(zboot_tool);
 }
+
+const Example = struct {
+    target: std.zig.CrossTarget,
+    name: []const u8,
+    root_file: []const u8,
+    linker_script: []const u8,
+};
---
+++ src/app_main.zig
@@ -0,0 +1,14 @@
+const hal = @import("hal/hal.zig");
+const sys = @import("platform/sys.zig");
+
+export fn main() noreturn {
+    hal.clock.clock_init();
+    sys.zconfig.probe_extconfig(sys.get_rom_end());
+    const zboot_config = sys.zconfig.get_config();
+    if (zboot_config.uart.enable) {
+        sys.init_debug(zboot_config.uart.tx[0..]) catch {};
+    }
+    sys.debug.print("Hello Zboot App v1!\r\n", .{}) catch {};
+
+    while (true) {}
+}
